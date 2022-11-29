6386dcec3bc283ce090e0bc52112fbc716a3d5a7
Flamiks <119279748+Flamiks@users.noreply.github.com>
2022-11-29 07:41:07 +0300

    Add files via upload

---
+++ smsBOMBER300.py
@@ -0,0 +1,19 @@
+
+#   Version 12.1
+import requests, random, datetime, sys, time, argparse, os , colorama ,pickle
+from colorama import *
+from Banners import *
+from ServesSMS import *
+from Message import *
+from Function import *
+init()
+
+os.system("title sms-BOMBER-300")
+version = 'Бета'
+
+#Баннер cпамера
+check_update()
+time.sleep(1.8)
+OSystem()
+start()
+
